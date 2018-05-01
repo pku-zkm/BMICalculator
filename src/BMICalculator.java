42b04a9313fb0308f61f578493a5e3c2c7f0d2e7
Kemeng Zhu <zkm@pku.edu.cn>
2018-05-02 03:40:33 +0800

    Finished

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -15,19 +15,18 @@ public class BMICalculator extends JFrame {
             panels[i].add(labels[i]);
         }
 
-
         rbGender = new JRadioButton[]{new JRadioButton("男", true),
                 new JRadioButton("女")};
         bgGender = new ButtonGroup();
         for (JRadioButton radioButton : rbGender) {
+            radioButton.setFont(fontLabel);
             bgGender.add(radioButton);
             panels[0].add(radioButton);
         }
 
-        tfHeight = new JTextField(10);
+        tfHeight = new JTextField(16);
         panels[1].add(tfHeight);
-
-        tfWeight = new JTextField(10);
+        tfWeight = new JTextField(16);
         panels[2].add(tfWeight);
 
         cbUnit = new JComboBox[]{new JComboBox<String>(), new JComboBox<String>()};
@@ -35,6 +34,9 @@ public class BMICalculator extends JFrame {
         cbUnit[0].addItem("厘米(cm)");
         cbUnit[1].addItem("公斤(kg)");
         cbUnit[1].addItem("斤");
+        Font fontSong = new Font("宋体", Font.PLAIN, 14);
+        cbUnit[0].setFont(fontSong);
+        cbUnit[1].setFont(fontSong);
         panels[1].add(cbUnit[0]);
         panels[2].add(cbUnit[1]);
 
@@ -44,22 +46,30 @@ public class BMICalculator extends JFrame {
         }
 
         JPanel pnlCalc = new JPanel(new BorderLayout());
-        pnlCalc.setBorder(new EmptyBorder(5, 5, 5, 5));
+        pnlCalc.setBorder(new EmptyBorder(3, 5, 5, 7));
         btnCalc = new JButton("计算");
+        btnCalc.setFont(fontSong);
         btnCalc.addActionListener(e -> calc());
         pnlCalc.add(btnCalc, BorderLayout.EAST);
         getContentPane().add(pnlCalc);
+
+        Font fontTf = new Font("Monospaced", Font.PLAIN, 14);
+        tfHeight.setFont(fontTf);
+        tfWeight.setFont(fontTf);
 
         lblResult = new JLabel();
         lblResult.setFont(new Font("宋体", Font.PLAIN, 16));
         pnlCalc.add(lblResult, BorderLayout.CENTER);
 
-
-        setSize(270, 170);
+        setSize(292, 165);
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     private void calc() {
+        if (tfHeight.getText().length() < 1 || tfWeight.getText().length() < 1) {
+            return;
+        }
         double height = Double.parseDouble(tfHeight.getText());
         double weight = Double.parseDouble(tfWeight.getText());
         if (cbUnit[0].getSelectedIndex() == 1) {
@@ -70,14 +80,30 @@ public class BMICalculator extends JFrame {
         }
         double bmi = weight / (height * height);
         String comment;
-        if (bmi < 18.5) {
-            comment = "过轻";
-        } else if (bmi < 24) {
-            comment = "正常";
-        } else if (bmi < 28) {
-            comment = "偏胖";
+        if (rbGender[0].isSelected()) {
+            if (bmi < 20) {
+                comment = "过轻";
+            } else if (bmi < 25) {
+                comment = "正常";
+            } else if (bmi < 30) {
+                comment = "偏胖";
+            } else if (bmi < 35) {
+                comment = "肥胖";
+            } else {
+                comment = "非常肥胖";
+            }
         } else {
-            comment = "肥胖";
+            if (bmi < 19) {
+                comment = "过轻";
+            } else if (bmi < 24) {
+                comment = "正常";
+            } else if (bmi < 29) {
+                comment = "偏胖";
+            } else if (bmi < 34) {
+                comment = "肥胖";
+            } else {
+                comment = "非常肥胖";
+            }
         }
         SwingUtilities.invokeLater(() ->
                 lblResult.setText(String.format("您的BMI是%.1f，%s。", bmi, comment)));
@@ -95,5 +121,4 @@ public class BMICalculator extends JFrame {
     private JRadioButton[] rbGender;
     private ButtonGroup bgGender;
     private JComboBox[] cbUnit;
-
 }
